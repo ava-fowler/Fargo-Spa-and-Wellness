4e6cdef1196cb70fca31713e5f31b89bc1eaabf6
mercydinukar2-stack <mercydinukar2@gmail.com>
2025-12-05 11:22:49 -0600

    add more classes

---
+++ Fargo Spa and Wellness/Appointment.cs
@@ -0,0 +1,14 @@
+﻿namespace FargoSpaandWellness
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ServiceId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; }
+        // Navigation properties
+        public User User { get; set; }
+        public Service Service { get; set; }
+    }
+}
