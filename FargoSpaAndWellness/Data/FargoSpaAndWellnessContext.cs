9d124ce1813438220d029a2260529779e22286a7
mercydinukar2-stack <mercydinukar2@gmail.com>
2025-12-10 14:13:38 -0600

    made the login and register

---
+++ FargoSpaAndWellness/Data/FargoSpaAndWellnessContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FargoSpaAndWellness.Data;
+
+namespace FargoSpaAndWellness.Data
+{
+    public class FargoSpaAndWellnessContext(DbContextOptions<FargoSpaAndWellnessContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
